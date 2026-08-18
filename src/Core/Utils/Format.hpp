e7ae284eb33a42f5b5bd80cf1895bda17e9e1b01
vasyakaban <kirill.balashov03@gmail.com>
2026-08-18 17:07:10 +0300

    Some math and format

--- src/Core/Utils/Format.hpp
+++ src/Core/Utils/Format.hpp
@@ -3,24 +3,28 @@
 #include "Types.hpp"
 #include "Memory.h"
 #include "String.h"
-#include "CommonExceptions.h"
+#include "StringView.h"
 
 namespace Core
 {
     template<typename T>
-    struct Formatter
-    {
-        Formatter(const T& value);
-        DeviceSize GetSize();
-        Void Format(UTF8Char* output);
+    struct Formatter;
+
+    template<typename T>
+    concept Formattable = requires(T& obj, Formatter<T>& fmt, UTF8Char* output) {
+        Formatter<T>(Forward(obj));
+        { fmt.GetSize() } -> SameAs<DeviceSize>;
+        { fmt.Format(output) } -> SameAs<Void>;
     };
 
     namespace Detail
     {
         template<typename T, typename... Args>
-        String FormatImpl(Allocator allocator, DeviceSize size, const T& arg, const Args&... args)
+        String FormatImpl(Allocator& allocator, DeviceSize size, T&& arg, Args&&... args)
         {
-            Formatter<T> fmt(arg);
+            //if we pass actual Formatter<...>(for example in case of Core::Fmt) then we should use it instead of creating Formatter<Formatter<...>>
+            using ArgType = DropConstVolatileReference<T>;
+            Conditional<TypeInstantiation<ArgType, Formatter>, ArgType, Formatter<ArgType>> fmt(Forward(arg));
             DeviceSize new_size = size + fmt.GetSize();
 
             if constexpr(sizeof...(args) == 0)
@@ -31,7 +35,7 @@ namespace Core
             }
             else
             {
-                auto str = FormatImpl(allocator, new_size, args...);
+                auto str = FormatImpl(allocator, new_size, Forward(args)...);
                 fmt.Format(str.GetData() + size);
                 return str;
             }
@@ -39,90 +43,171 @@ namespace Core
     };
 
     template<typename... Args>
-    String Format(Allocator allocator, const Args&... args)
+    requires(Formattable<DropConstVolatileReference<Args>> && ...)
+    String Format(Allocator allocator, Args&&... args)
     {
         if constexpr(sizeof...(args) == 0)
             return String(allocator);
         else
-            return Detail::FormatImpl(allocator, 0, args...);
+            return Detail::FormatImpl(allocator, 0, Forward(args)...);
     }
 
     template<typename... Args>
-    String Format(const Args&... args)
+    String Format(Args&&... args)
     {
         return Format(GetGlobalAllocator(), Forward(args)...);
     }
 
-    //for integers
-    template<Integral I>
-    struct Formatter<I>
-    {
-        static_assert(NumericLimits<UInt64>::Max == 18'446'744'073'709'551'615ull); //6 * 3 + 2 = 20
+    //explicit formatters
 
-        Formatter(const I& value)
-        {
-            this->length = 0;
-            if(value == 0)
-                this->length = 1;
-            else
-            {
-                if constexpr(SignedIntegral<I>)
-                {
-                    if(value < 0)
-                        this->length++;
-                }
-
-                I value_copy = value;
-
-                while(value_copy != 0)
-                {
-                    this->length++;
-                    value_copy /= 10;
-                }
-            }
-        }
-
-        DeviceSize GetSize()
-        {
-            return this->length;
-        }
-
-        Void Format(UTF8Char* output)
-        {
-            CopyNonOverlappedMemory(this->data, output, this->length);
-        }
-
-        UTF8Char data[20];
-        UInt8 length;
-    };
-
-    //for char arrays
+    //strings
     template<Character C, DeviceSize N>
     struct Formatter<const C[N]>
     {
-        Formatter(const C (&data)[N])
-            : data(data),
-              size(N - 1)
-        {
-            auto res = StringEncoder::GetLength(data, size);
-            if(res.input_offset != size)
-                throw StringEncoderBadCharacterException(this->size, res.input_offset);
+        constexpr static DeviceSize Size = N - 1;
 
-            this->output_size = res.output_size;
+        const C* str;
+
+        Formatter(const C (&str)[N])
+            : str(str)
+        {}
+
+        DeviceSize GetSize() noexcept
+        {
+            return StringEncoder::GetLength(this->str, Size).output_size;
         }
 
-        DeviceSize GetSize()
+        Void Format(UTF8Char* output) noexcept
         {
-            return output_size;
+            StringEncoder::Convert(this->str, Size, output);
         }
-
-        Void Format(UTF8Char* output)
-        {
-            StringEncoder::Convert(this->data, this->size, output);
-        }
-
-        const C* data;
-        DeviceSize size;
-        DeviceSize output_size;
     };
+
+    //integers
+    template<Integral I>
+    struct Formatter<I>
+    {
+        //18'446'744'073'709'551'615 -> 6 * 3 + 2 = 20 chars
+        //-9'223'372'036'854'775'808
+
+        constexpr static DeviceSize BufferSize = 20;
+
+        UTF8Char buffer[BufferSize];
+        DeviceSize start;
+
+        Formatter(I value)
+        {
+            Bool is_negative = (value < 0);
+
+            Int8 char_pos = BufferSize - 1;
+            for(; value != 0; value /= 10)
+            {
+                UTF8Char ch = u8'0' + (value % 10);
+                this->buffer[char_pos--] = ch;
+            }
+
+            if(is_negative)
+                this->buffer[char_pos--] = u8'-';
+
+            this->start = char_pos + 1;
+        }
+
+        DeviceSize GetSize() noexcept
+        {
+            return BufferSize - this->start;
+        }
+
+        Void Format(UTF8Char* output) noexcept
+        {
+            CopyNonOverlappedMemory(buffer + this->start, output, GetSize());
+        }
+    };
+
+    //integers
+    template<Pointer P>
+    struct Formatter<P>
+    {
+        constexpr static DeviceSize BufferSize = sizeof(DeviceSize) * 2 + 2; //0x12 34 56 78 90 AB CD EF
+
+        UTF8Char buffer[BufferSize];
+        DeviceSize start;
+
+        Formatter(P ptr)
+        {
+            DeviceSize int_ptr = reinterpret_cast<DeviceSize>(ptr);
+
+            Int8 char_pos = BufferSize - 1;
+            for(; int_ptr != 0; int_ptr /= 16)
+            {
+                auto digit = int_ptr % 16;
+
+                UTF8Char ch = (digit < 10 ? u8'0' + digit : u8'a' + (digit - 10));
+                this->buffer[char_pos--] = ch;
+            }
+
+            this->buffer[char_pos--] = u8'x';
+            this->buffer[char_pos--] = u8'0';
+
+            this->start = char_pos + 1;
+        }
+
+        DeviceSize GetSize() noexcept
+        {
+            return BufferSize - this->start;
+        }
+
+        Void Format(UTF8Char* output) noexcept
+        {
+            CopyNonOverlappedMemory(buffer + this->start, output, GetSize());
+        }
+    };
+
+    //StringView
+    template<>
+    struct Formatter<StringView>
+    {
+        StringView view;
+
+        Formatter(StringView view)
+            : view(view)
+        {}
+
+        DeviceSize GetSize() noexcept
+        {
+            return this->view.GetSize();
+        }
+
+        Void Format(UTF8Char* output) noexcept
+        {
+            CopyNonOverlappedMemory(this->view.GetData(), output, GetSize());
+        }
+    };
+
+    //String
+    template<>
+    struct Formatter<String>
+    {
+        StringView view;
+
+        Formatter(const String& str)
+            : view(str)
+        {}
+
+        DeviceSize GetSize() noexcept
+        {
+            return this->view.GetSize();
+        }
+
+        Void Format(UTF8Char* output) noexcept
+        {
+            CopyNonOverlappedMemory(this->view.GetData(), output, GetSize());
+        }
+    };
+
+    template<typename T>
+    requires Formattable<DropConstVolatileReference<T>>
+    auto Fmt(T&& obj) noexcept(NoexceptConstructible<Formatter<DropConstVolatileReference<T>>, T>)
+    {
+        return Formatter<DropConstVolatileReference<T>>(Forward(obj));
+    }
 };
--- src/Core/Utils/Math.hpp
+++ src/Core/Utils/Math.hpp
@@ -1,11 +1,143 @@
 #pragma once
 
+#include <math.h>
 #include "Types.hpp"
 #include "Traits.hpp"
 #include "Utility.hpp"
 
 namespace Core
 {
+    constexpr inline Float64 Pi = 3.1415926535897932384626433832795;
+
+    template<FloatingPoint F>
+    constexpr F DegreesToRadians(F deg) noexcept
+    {
+        return (deg * static_cast<F>(Pi)) / static_cast<F>(180.0);
+    }
+
+    template<FloatingPoint F>
+    constexpr F RadiansToDegrees(F rad) noexcept
+    {
+        return (rad * static_cast<F>(180.0)) / static_cast<F>(Pi);
+    }
+
+    template<FloatingPoint F>
+    F Sine(F rads) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return sinf(rads);
+        else
+            return sin(rads);
+    }
+
+    template<FloatingPoint F>
+    F Cosine(F rads) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return cosf(rads);
+        else
+            return cos(rads);
+    }
+
+    template<FloatingPoint F>
+    F Tangent(F rads) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return tanf(rads);
+        else
+            return tan(rads);
+    }
+
+    template<FloatingPoint F>
+    F ArcSine(F sin) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return asinf(sin);
+        else
+            return asin(sin);
+    }
+
+    template<FloatingPoint F>
+    F ArcCosine(F cos) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return acosf(cos);
+        else
+            return acos(cos);
+    }
+
+    template<FloatingPoint F>
+    F ArcTangent(F tan) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return atanf(tan);
+        else
+            return atan(tan);
+    }
+
+    template<FloatingPoint F>
+    F SquareRoot(F value) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return sqrtf(value);
+        else
+            return sqrt(value);
+    }
+
+    template<FloatingPoint F>
+    F Absolute(F value) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return fabsf(value);
+        else
+            return fabs(value);
+    }
+
+    template<FloatingPoint F>
+    F Power(F base, F exponent) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return powf(base, exponent);
+        else
+            return pow(base, exponent);
+    }
+
+    template<FloatingPoint F>
+    F Ceil(F value) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return ceilf(value);
+        else
+            return ceil(value);
+    }
+
+    template<FloatingPoint F>
+    F Floor(F value) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return floorf(value);
+        else
+            return floor(value);
+    }
+
+    template<FloatingPoint F>
+    F Round(F value) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return roundf(value);
+        else
+            return round(value);
+    }
+
+    template<FloatingPoint F>
+    F Truncate(F value) noexcept
+    {
+        if constexpr(SameAs<F, Float32>)
+            return truncf(value);
+        else
+            return trunc(value);
+    }
+
     template<typename T, typename C>
     requires InvocableWithResult<Bool, C, const T&, const T&>
     constexpr const T& Clamp(const T& min, const T& max, const T& value, C&& comparator) noexcept(NoexceptInvocableWithResult<Bool, C, const T&, const T&>)
